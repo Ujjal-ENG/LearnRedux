2acba6c21831c89d48d28e41f894b3506d74ca71
user <ujjalroy7862@gmail.com>
2022-12-11 22:14:11 +0600

    here i learned, how the payload word on different data

--- index.js
+++ index.js
@@ -9,10 +9,21 @@ const { createStore } = require("redux");
 const INCREMENT = "INCREMENT";
 const DECREMENT = "DECREMENT";
 const RESET = "RESET";
+const INCREMENT_BY_VALUE = "INCREMENT_BY_VALUE";
+const ADD_USER = "ADD_USER"
 
 const initialState = {
+  users: ["ujjal "],
   count: 0,
 };
+
+
+const addUser = (value) => {
+  return {
+    type: ADD_USER,
+    payload: value
+  }
+}
 
 const incrementCounterAction = () => {
   return {
@@ -32,6 +43,13 @@ const resetCounterAction = () => {
   };
 };
 
+const incrementCounterByValue = (value) => {
+  return {
+    type: INCREMENT_BY_VALUE,
+    payload: value,
+  };
+};
+
 //Now Create The Reducer
 // We know reducer take to parameter on is state and one is action
 
@@ -48,29 +66,44 @@ const counterReducer = (state = initialState, action) => {
         ...state,
         count: state.count - 1,
       };
+
     case RESET:
       return {
         ...state,
         count: 0,
       };
 
+    case INCREMENT_BY_VALUE:
+      return {
+        ...state,
+        count: state.count + action.payload,
+      };
+    
+    case ADD_USER:
+      return {
+        users: [...state.users, action.payload],
+        count: state.count + 6
+      }
+
     default:
-      state
+      state;
   }
 };
 
-
 //store
 
-const store = createStore(counterReducer)
+const store = createStore(counterReducer);
 
 store.subscribe(() => {
-  console.log(store.getState())
-})
+  console.log(store.getState());
+});
 
-store.dispatch(incrementCounterAction())
-store.dispatch(incrementCounterAction())
-store.dispatch(incrementCounterAction())
-store.dispatch(decrementCounterAction())
-store.dispatch(decrementCounterAction())
-store.dispatch(resetCounterAction())
+store.dispatch(incrementCounterAction());
+store.dispatch(incrementCounterAction());
+store.dispatch(incrementCounterAction());
+store.dispatch(decrementCounterAction());
+store.dispatch(incrementCounterByValue(10));
+store.dispatch(decrementCounterAction());
+store.dispatch(resetCounterAction());
+
+store.dispatch(addUser("Ador"))
