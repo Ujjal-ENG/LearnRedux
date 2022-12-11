8f5ff232350ae44d4a551105ccb1f4e27e10bb0a
user <ujjalroy7862@gmail.com>
2022-12-11 20:34:22 +0600

    create the reducer function to see how mainly work redux

--- index.js
+++ index.js
@@ -1,34 +1,40 @@
 //State
 const initialCounterState = {
   count: 0,
-}
-
-const initialUsersState = {
-  users: [
-    {
-      name: "Ujjal Roy"
-    }
-  ]
-}
+  Error: {
+    type: Error,
+    default: "Something is Wrong there...",
+  },
+};
 
 //Action -Object -Type or Payload
-
 const incrementCounter = () => {
   return {
-    type: "INCREMENT"
-  }
-} 
+    type: "INCREMENT",
+  };
+};
 
 const decrementCounter = () => {
   return {
-    type: "DECREMENT"
-  }
-}
+    type: "DECREMENT",
+  };
+};
 
-const addUser = () => {
-  return {
-    type: "ADD_USER",
-    payload: { name: 'Tamim'}
-  }
-}
+//Create the reducer for counter
+const counterReducer = (state = initialCounterState, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+    case "DECREMENT":
+      return {
+        ...state,
+        count: state.count - 1,
+      };
 
+    default:
+      break;
+  }
+};
