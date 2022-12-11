b0e1451e373729e6aa585c40c6f7fdab9b1df8d9
user <ujjalroy7862@gmail.com>
2022-12-12 03:00:16 +0600

    here i leraned how to combined two reducer using combinedReducer function

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-const { createStore } = require("redux");
+const { createStore, combineReducers } = require("redux");
 
-//Constraints
+//Product Constraints
 const GET_PRODUCTS = "GET_PRODUCTS";
 const ADD_PRODUCTS = "ADD_PRODUCTS";
 
-// productReducer
+//Cart Constraints
+const GET_CARTS = "GET_CARTS";
+const ADD_CARTS = "ADD_CARTS";
+
+// productStates
 const initialProductState = {
   products: ["sugar", "salt"],
   numberofProducts: 2,
+};
+
+// CARTStates
+const initialCartState = {
+  cart: ["sugar"],
+  numberofProducts: 1,
 };
 
 //action of Product State
@@ -24,7 +34,21 @@ const addProducts = (product) => {
   };
 };
 
-//Reducer of Action Performed
+//action of Cart State
+const getCARTs = () => {
+  return {
+    type: GET_CARTS,
+  };
+};
+
+const addCARTs = (cart) => {
+  return {
+    type: ADD_CARTS,
+    payload: cart,
+  };
+};
+
+//Product Reducer of Action Performed
 const productReducer = (state = initialProductState, action) => {
   switch (action.type) {
     case GET_PRODUCTS:
@@ -38,16 +62,53 @@ const productReducer = (state = initialProductState, action) => {
       };
 
     default:
-      state;
+      return state;
   }
 };
 
-const store = createStore(productReducer);
+//Cart Reducer of Action Performed
+const cartReducer = (state = initialCartState, action) => {
+  switch (action.type) {
+    case GET_CARTS:
+      return {
+        ...state,
+      };
+    case ADD_CARTS:
+      return {
+        cart: [...state.cart, action.payload],
+        numberofProducts: state.numberofProducts + 1,
+      };
+
+    default:
+      return state;
+  }
+};
+
+//This combined Reducer
+const rootReducer = combineReducers({
+  productR: productReducer,
+  cartR: cartReducer,
+});
+
+//Product Stroe
+// const store = createStore(productReducer);
+// store.subscribe(() => {
+//   console.log(store.getState());
+// });
+
+// store.dispatch(getProducts());
+// store.dispatch(addProducts("orange"));
+
+//Combined Store
+const store = createStore(rootReducer);
 store.subscribe(() => {
   console.log(store.getState());
 });
 
 store.dispatch(getProducts());
 store.dispatch(addProducts("orange"));
+
+store.dispatch(getCARTs());
+store.dispatch(addCARTs("salt"));
 
 //cardReducer
