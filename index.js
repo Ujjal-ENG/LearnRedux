9af4e7c5eb3bc59c04884e31a93f72ec91bad012
user <ujjalroy7862@gmail.com>
2022-12-12 02:49:28 +0600

    practice redux

--- index.js
+++ index.js
@@ -1,109 +1,53 @@
-//State - count=0
-//action - increment,decrement,reset
-//reducer
-//store
-
 const { createStore } = require("redux");
 
-//CONSTRAINTS
-const INCREMENT = "INCREMENT";
-const DECREMENT = "DECREMENT";
-const RESET = "RESET";
-const INCREMENT_BY_VALUE = "INCREMENT_BY_VALUE";
-const ADD_USER = "ADD_USER"
+//Constraints
+const GET_PRODUCTS = "GET_PRODUCTS";
+const ADD_PRODUCTS = "ADD_PRODUCTS";
 
-const initialState = {
-  users: ["ujjal "],
-  count: 0,
+// productReducer
+const initialProductState = {
+  products: ["sugar", "salt"],
+  numberofProducts: 2,
 };
 
-
-const addUser = (value) => {
+//action of Product State
+const getProducts = () => {
   return {
-    type: ADD_USER,
-    payload: value
-  }
-}
-
-const incrementCounterAction = () => {
-  return {
-    type: INCREMENT,
+    type: GET_PRODUCTS,
   };
 };
 
-const decrementCounterAction = () => {
+const addProducts = (product) => {
   return {
-    type: DECREMENT,
+    type: ADD_PRODUCTS,
+    payload: product,
   };
 };
 
-const resetCounterAction = () => {
-  return {
-    type: RESET,
-  };
-};
-
-const incrementCounterByValue = (value) => {
-  return {
-    type: INCREMENT_BY_VALUE,
-    payload: value,
-  };
-};
-
-//Now Create The Reducer
-// We know reducer take to parameter on is state and one is action
-
-const counterReducer = (state = initialState, action) => {
+//Reducer of Action Performed
+const productReducer = (state = initialProductState, action) => {
   switch (action.type) {
-    case INCREMENT:
+    case GET_PRODUCTS:
       return {
         ...state,
-        count: state.count + 1,
       };
-
-    case DECREMENT:
+    case ADD_PRODUCTS:
       return {
-        ...state,
-        count: state.count - 1,
+        products: [...state.products, action.payload],
+        numberofProducts: state.numberofProducts + 1,
       };
-
-    case RESET:
-      return {
-        ...state,
-        count: 0,
-      };
-
-    case INCREMENT_BY_VALUE:
-      return {
-        ...state,
-        count: state.count + action.payload,
-      };
-    
-    case ADD_USER:
-      return {
-        users: [...state.users, action.payload],
-        count: state.count + 6
-      }
 
     default:
       state;
   }
 };
 
-//store
-
-const store = createStore(counterReducer);
-
+const store = createStore(productReducer);
 store.subscribe(() => {
   console.log(store.getState());
 });
 
-store.dispatch(incrementCounterAction());
-store.dispatch(incrementCounterAction());
-store.dispatch(incrementCounterAction());
-store.dispatch(decrementCounterAction());
-store.dispatch(incrementCounterByValue(10));
-store.dispatch(decrementCounterAction());
-store.dispatch(resetCounterAction());
+store.dispatch(getProducts());
+store.dispatch(addProducts("orange"));
 
-store.dispatch(addUser("Ador"))
+//cardReducer
