cbe041bdee1f3dc5d7640bd8935c05b32ca80cd2
user <ujjalroy7862@gmail.com>
2022-12-13 00:46:29 +0600

    make the reducer function of my project

--- ReduxProject1.jsx
+++ ReduxProject1.jsx
@@ -39,3 +39,29 @@ const getTodoFailed = (error) => {
 
 
 //reducers
+const todosReducer = (state=initialTodoState, action) => {
+  switch (action.type) {
+    case GET_TODOS_REQUEST:
+      return {
+        ...state,
+        isLoading: true,
+      }
+    
+    case GET_TODOS_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        todos: action.payload
+      }
+    
+    case GET_TODOS_FAILED:
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload
+      }
+  
+    default:
+      state
+  }
+}
