13490e0c361aea0e78458d1087f64ab9cc35840b
user <ujjalroy7862@gmail.com>
2022-12-16 15:56:29 +0600

    complete the todosActionReducer implement

--- react-redux/src/services/actions/todoAction.js
+++ react-redux/src/services/actions/todoAction.js
@@ -1,7 +1,24 @@
 import axios from "axios";
+import {
+  GET_TODOS_FAILED,
+  GET_TODOS_REQUEST,
+  GET_TODOS_SUCCESS,
+} from "../constants/todosContants";
 
-const getAllTodos = async (dispatch) => {
+export const getAllTodos = async (dispatch) => {
   dispatch({
-    type: 
-  })
-}
+    type: GET_TODOS_REQUEST,
+  });
+  try {
+    const res = await axios.get("https://jsonplaceholder.typicode.com/todos");
+    dispatch({
+      type: GET_TODOS_SUCCESS,
+      payload: res.data,
+    });
+  } catch (error) {
+    dispatch({
+      type: GET_TODOS_FAILED,
+      payload: error.message,
+    });
+  }
+};
