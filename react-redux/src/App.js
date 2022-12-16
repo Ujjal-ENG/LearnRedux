b72428cec4cc99f755ce652f129a330b68c9e75b
user <ujjalroy7862@gmail.com>
2022-12-16 19:25:02 +0600

    done this app

--- react-redux/src/App.js
+++ react-redux/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import CounterView from "./features/counter/CounterView";
+import TodoView from "./features/todos/TodoView";
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to React-Redux</h1>
       <CounterView />
+      <TodoView />
     </div>
   );
 }
--- react-redux/src/features/todos/TodoView.jsx
+++ react-redux/src/features/todos/TodoView.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchTodos } from "./todosSlice";
 
 const TodoView = () => {
-  return (
-    <div>TodoView</div>
-  )
-}
+  const { isLoading, todos, error } = useSelector((state) => state.todos);
 
-export default TodoView
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchTodos());
+  }, []);
+
+  return (
+    <>
+      <h1>Here Is Our Todos Man</h1>
+      {isLoading && <h3>Loading...</h3>}
+      {error && <h3>{error.message}...</h3>}
+      {todos &&
+        todos.map((todo) => {
+          return (
+            <>
+              <article key={Math.random()}>
+                <h3>{todo.title}</h3>
+                <p>{todo.posts}</p>
+              </article>
+            </>
+          );
+        })}
+    </>
+  );
+};
+
+export default TodoView;
