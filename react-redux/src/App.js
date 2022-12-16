4835526d7eae9c712eb317aa4cc9336cc9c3e1f0
user <ujjalroy7862@gmail.com>
2022-12-16 18:39:50 +0600

    create the counter app using redux-toolkit

--- react-redux/src/App.js
+++ react-redux/src/App.js
@@ -1,12 +1,11 @@
 import "./App.css";
 import Counter from "./components/Counter";
-import Todos from "./components/Todos";
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to React-Redux</h1>
-      <Todos />
+      <Counter />
     </div>
   );
 }
--- react-redux/src/store.js
+++ react-redux/src/store.js
@@ -1,9 +1,9 @@
-import { applyMiddleware, createStore } from "redux";
-import thunk from "redux-thunk";
-// import counterReducer from "./services/reducers/counterReducer";
-import todosReducer from "./services/reducers/todosReducer.js";
+import { createStore } from "redux";
+// import thunk from "redux-thunk";
+import counterReducer from "./services/reducers/counterReducer";
+// import todosReducer from "./services/reducers/todosReducer.js";
 
-// const store = createStore(counterReducer);
-const store = createStore(todosReducer, applyMiddleware(thunk));
+const store = createStore(counterReducer);
+// const store = createStore(todosReducer, applyMiddleware(thunk));
 
 export default store;
